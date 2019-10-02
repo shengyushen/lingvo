03bc113f5ec22c0ba5c24da8cb8011615b0972bf
shengyushen <shengyushen@aliyun.com>
2019-10-02 02:33:38 +0000

    run on 4*v100

--- lmgpipe.sh
+++ lmgpipe.sh
@@ -1,13 +1,12 @@
 #  https://github.com/tensorflow/lingvo/issues/48 tell us about the gpu setting
 
 LOGDIR=/tmp/mnist/log
-GPU_NUM=1
+GPU_NUM=4
 
 # must first delete the log dir
 rm -rf ${LOGDIR}
 
 #nvprof -f --quiet --profile-api-trace none --print-gpu-trace --print-nvlink-topology --csv --log-file lmgpipe.csv -o lmgpipe.nvvp \
-#bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr --controller_gpus=${GPU_NUM} --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
-#bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr --controller_gpus=${GPU_NUM} --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
- bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr                              --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
+ bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr --controller_gpus=${GPU_NUM} --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
+#bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr                              --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
 
