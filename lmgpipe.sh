31aeea47d6d15d1f5ae13dcd9efefc82f8a89d58
Ubuntu <ubuntu@ip-172-31-46-17.ap-northeast-1.compute.internal>
2019-10-02 08:03:37 +0000

    finally capture log with nvprof

--- lmgpipe.sh
+++ lmgpipe.sh
@@ -6,7 +6,6 @@ GPU_NUM=4
 # must first delete the log dir
 rm -rf ${LOGDIR}
 
-#nvprof -f --quiet --profile-api-trace none --print-gpu-trace --print-nvlink-topology --csv --log-file lmgpipe.csv -o lmgpipe.nvvp \
+nvprof -f --quiet --profile-api-trace none --print-gpu-trace --print-nvlink-topology --csv --log-file lmgpipe.csv -o lmgpipe.nvvp \
  bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr --controller_gpus=${GPU_NUM} --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
-#bazel-bin/lingvo/trainer --run_locally=gpu --mode=sync --model=lm.one_billion_wds.OneBWdsGPipeTransformerWPM --logdir=${LOGDIR} --logtostderr                              --worker_gpus=${GPU_NUM} --worker_split_size=${GPU_NUM} > outxx${GPU_NUM} 2> errxx${GPU_NUM}
 
